d9660e472a01c6d11e4a000197bc073f707770ef
Maciej Matczak <mmatczak+github.gmail.com>
2024-07-27 21:33:38 +0200

    Add rotating import info save, add sugar coating with awful amount of unnecessary spinners

--- photo_import/cmd.py
+++ photo_import/cmd.py
@@ -4,9 +4,10 @@ import shutil
 import subprocess as sp
 import sys
 import typing as t
-from collections import defaultdict
+from collections import defaultdict, deque
 from dataclasses import dataclass
 from pathlib import Path
+from time import sleep
 
 import click
 import psutil
@@ -14,6 +15,8 @@ import questionary
 from psutil._common import bytes2human
 from pydantic import BaseModel, ValidationError, field_validator
 from rich import print
+from rich.console import Console
+from rich.panel import Panel
 from rich.pretty import pprint
 from yaml import safe_load
 
@@ -21,7 +24,11 @@ ROOT = Path(__file__).resolve().parent.parent
 APP_NAME = "mm-photo-import"
 
 
+console = Console()
+
+
 class Scenario(BaseModel):
+    folder: str
     source: Path
 
 
@@ -103,9 +110,26 @@ def scan_source_dir(path: Path, from_: datetime.datetime, to: datetime.datetime)
     return ScanResult(found_extensions=found_extensions, oldest=oldest, newest=newest)
 
 
+def write_and_rotate(path: Path, line, max_lines=10):
+    try:
+        raw = path.read_text().splitlines()
+    except FileNotFoundError:
+        raw = list()
+
+    content = deque(raw, max_lines)
+    content.append(line)
+    path.write_text("\n".join(content))
+
+
+def spinner(msg=""):
+    with console.status(msg):
+        sleep(1)
+
+
 @dataclass
 class App:
     app_dir: Path
+    import_sessions_dir: Path
     app_config: AppConfig
     app_config_path: Path
     user_config: UserConfig
@@ -115,17 +139,21 @@ class App:
 @click.group(invoke_without_command=True)
 @click.pass_context
 def cmd(ctx):
-    print("[blue]Setting up")
+    print(Panel("[bold blue]Setting up"))
     print("")
 
     app_dir = Path(click.get_app_dir(APP_NAME))
     app_dir.mkdir(exist_ok=True)
+    import_sessions_dir = app_dir / "import-sessions"
+    import_sessions_dir.mkdir(exist_ok=True)
 
-    print(f"[magenta]App directory:[/] {app_dir}")
+    print(f"[magenta]App directory[/]: {app_dir}")
 
     app_config_path = app_dir / "config.yml"
 
-    print(f"[magenta]App config path:[/] {app_config_path}")
+    print(f"[magenta]App config path[/]: {app_config_path}")
+
+    spinner("reading app config...")
 
     if not app_config_path.exists():
         print("[red]App config doesn't exist!")
@@ -141,6 +169,8 @@ def cmd(ctx):
     print("[magenta]App config:")
     pprint(app_config.model_dump(mode="json"), expand_all=True)
 
+    spinner("reading user config...")
+
     try:
         user_config = load_config(app_config.user_config, UserConfig)
     except ValidationError as exception:
@@ -149,11 +179,12 @@ def cmd(ctx):
         sys.exit(1)
 
     print("")
-    print(f"[magenta]User config: [/] {app_config_path}")
+    print(f"[magenta]User config[/]: {app_config_path}")
     pprint(user_config.model_dump(mode="json"), expand_all=True)
 
     ctx.obj = App(
         app_dir=app_dir,
+        import_sessions_dir=import_sessions_dir,
         user_config=user_config,
         user_config_path=app_config.user_config,
         app_config=app_config,
@@ -161,11 +192,11 @@ def cmd(ctx):
     )
 
     if ctx.invoked_subcommand is not None:
+        spinner("preparing...")
         print("")
 
 
 @cmd.command(name="import")
-@click.option("-s", "--scenario", required=False)
 @click.option("-f", "--from", "from_", type=click.DateTime())
 @click.option("-t", "--to", type=click.DateTime())
 @click.option("-n", "--dry-run", is_flag=True, default=False)
@@ -174,11 +205,11 @@ def cmd(ctx):
 def import_(
     app: App,
     scenario: str,
-    from_: datetime.datetime,
-    to: datetime.datetime,
+    from_: datetime.datetime | None,
+    to: datetime.datetime | None,
     dry_run: bool,
 ):
-    print("[blue]Reading data")
+    print(Panel("[blue]Reading data"))
     print("")
 
     try:
@@ -189,7 +220,7 @@ def import_(
         )
         sys.exit(1)
 
-    target = app.user_config.target_root / scenario
+    target = app.user_config.target_root / scenario_data.folder
 
     options = {}
     for partition in psutil.disk_partitions():
@@ -200,6 +231,22 @@ def import_(
     choice = questionary.select(
         "Which storage device to import photos from?", choices=options
     ).ask()
+    if choice is None:
+        raise click.Abort
+
+    history_file = app.import_sessions_dir / scenario
+    try:
+        history = history_file.read_text().splitlines()
+    except FileNotFoundError:
+        history = list()
+
+    print("")
+    print("[magenta]History:")
+    for e in history:
+        print(f" - {e}")
+    print("")
+
+    spinner("calculating...")
 
     scenario_source = Path(options[choice]) / scenario_data.source
 
@@ -208,22 +255,21 @@ def import_(
         print(f"Review your config: [green]{app.user_config_path}")
         sys.exit(1)
 
-    print("")
-    print(f"scenario: [magenta]{scenario}")
-    print(f"source: [magenta]{scenario_source}")
-    print(f"target: [magenta]{target}")
-    print("")
+    print(f"[magenta]scenario[/]: {scenario}")
+    print(f"[magenta]source[/]: {scenario_source}")
+    print(f"[magenta]target[/]: {target}")
 
-    print("Scanning dir...\n")
+    spinner("scanning dir...")
     scan_result = scan_source_dir(scenario_source, from_, to)
 
+    print("")
     print("Found files:")
     for ext, count in scan_result.found_extensions.items():
-        print(f"  {ext}: [magenta]{count}")
+        print(f"  [magenta]{ext}[/]: {count}")
     o = scan_result.oldest.isoformat(sep=" ") if scan_result.oldest else "?"
     n = scan_result.newest.isoformat(sep=" ") if scan_result.newest else "?"
-    print(f"oldest: [magenta]{o}")
-    print(f"newest: [magenta]{n}")
+    print(f"[magenta]oldest[/]: {o}")
+    print(f"[magenta]newest[/]: {n}")
 
     print("")
 
@@ -242,7 +288,7 @@ def import_(
     click.confirm("Ok? Continue?", abort=True)
 
     print("")
-    print("[blue]Importing data")
+    print(Panel("[blue]Importing data"))
     print("")
 
     cmd = [
@@ -267,10 +313,27 @@ def import_(
         str(scenario_source),
         str(target),
     ]
+
+    spinner("rendering command...")
+
     print("Command:")
     print(" ", shlex.join(cmd))
     print("")
 
     click.confirm("Ok? Continue?", abort=True)
+    print("")
+
+    history_record = []
+    if from_:
+        history_record.append(f"from: {from_} (user)")
+    else:
+        history_record.append(f"from: {scan_result.oldest} (auto)")
+
+    if to:
+        history_record.append(f"to: {to} (user)")
+    else:
+        history_record.append(f"to: {scan_result.newest} (auto)")
+
+    write_and_rotate(history_file, ";  ".join(history_record))
 
     sp.run(cmd)
